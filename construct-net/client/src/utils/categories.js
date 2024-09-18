c22f5f2ef780ee86867688dfa7d8003cde2f11a1
Akshay  H R <ramakshay100@gmail.com>
2024-09-18 11:17:42 +0530

    service categories and firebase congiguration added

---
+++ construct-net/client/src/utils/categories.js
@@ -0,0 +1,12 @@
+export const categories = [
+  { name: "Architects", logo: "/service-1.svg" },
+  { name: "Interior Designers", logo: "/service-2.svg" },
+  { name: "Construction Workers", logo: "/service-3.svg" },
+  { name: "Safety Inspectors", logo: "/service-4.svg" },
+  { name: "Drone Surveying", logo: "/service-5.svg" },
+  { name: "3D Modeling", logo: "/service-6.svg" },
+  { name: "Project Managers", logo: "/service-7.svg" },
+  { name: "CAD Drafting", logo: "/service-8.svg" },
+  { name: "Consultants", logo: "/service-9.svg" },
+  { name: "Surveyors", logo: "/service-10.svg" },
+];
